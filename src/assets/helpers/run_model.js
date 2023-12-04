18b3ab59de8da52b10d3b0a9ba00ac2119f05ef6
anzeo <anze.oblak2@gmail.com>
2023-12-04 18:13:26 +0100

    Fixed loading the onnx model

--- src/assets/helpers/run_model.js
+++ src/assets/helpers/run_model.js
@@ -2,7 +2,7 @@ import { InferenceSession } from 'onnxjs';
 
 export async function loadModel() {
     const session = new InferenceSession();
-    await session.loadModel('./onnx_model.onnx');
+    await session.loadModel('./onnx_model.onnx');   // !!! The model MUST BE in ~/public directory, next to index.html !!!
     console.log("Session loaded")
     return session;
 }
